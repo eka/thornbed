2d73d22f334ebead8de0087323db2565fe92f20e
Esteban (Eka) Feldman <esteban.feldman@gmail.com>
2012-11-19 19:07:40 -0500

    QuickMeme implemented

---
+++ lib/thornbed/providers/quickmeme.rb
@@ -0,0 +1,40 @@
+require "uri"
+require "thornbed"
+
+
+module Thornbed::Providers
+  class QuickMeme < Thornbed::Providers::Provider
+    def initialize
+      @pattern = /^http(s)?:\/\/(i|t)?(\.)?(www\.)?(quickmeme.com|qkme.me)\/(meme\/)?(\w){6}(\/)?(.jpg)?(\?.*)?$/
+      @type = "photo"
+      @provider_url = "http://quickmeme.com"
+    end
+
+    def get(url)
+      raise Thornbed::NotValid, url if !self.valid?(url)
+      url = URI.parse(url)
+      normal = /quickmeme.com/ =~ url.to_s
+      direct = /qkme.me/ =~ url.to_s
+      ptype = "jpg"
+      if normal
+        pic_id = /(\w){6}/.match(url.path)
+      elsif direct
+        pic_id = url.path[/(\w){6}(.jpg)?/ =~ url.path..6]
+      else
+        raise Thornbed::NotValid, url
+      end
+
+      {
+        url: "http://i.qkme.me/#{pic_id}.#{ptype}",
+        type: "#{@type}",
+        provider_name: self.provider_name,
+        provider_url: @provider_url,
+        thumbnail_url: "http://t.qkme.me/#{pic_id}.#{ptype}",
+        version: "1.0",
+        page: "http://quickmeme.com/#{pic_id}/",
+        width: nil,
+        height: nil
+      }
+    end
+  end
+end
---
+++ spec/providers/quickmeme_spec.rb
@@ -0,0 +1,64 @@
+require "spec_helper"
+require "fakeweb"
+require "thornbed"
+
+describe "QuickMeme" do
+  it "should have QuickMeme as a provider" do
+    Thornbed::Providers.constants.include?(:QuickMeme).should_not be_false
+  end
+
+  it "should accept different quickmeme urls" do
+    quickmeme = Thornbed::Providers::QuickMeme.new
+    quickmeme.valid?('http://i.qkme.me/353dum.jpg').should be_true
+    quickmeme.valid?('http://t.qkme.me/353dum.jpg').should be_true
+    quickmeme.valid?('http://www.quickmeme.com/meme/353dum/').should be_true
+    quickmeme.valid?('http://qkme.me/353i1m?id=189959962').should be_true
+    quickmeme.valid?('http://qkme.me/353i1ma').should be_false
+    quickmeme.valid?('http://www.quickmeme.com/meme/353duma/').should be_false
+    quickmeme.valid?('http://i.qkme.me/353duma.jpg').should be_false
+  end
+
+  it "should return OEmbed hash for quickmeme urls" do
+    quickmeme = Thornbed::Providers::QuickMeme.new
+    res = quickmeme.get('http://i.qkme.me/353dum.jpg')
+    res.should include(url: 'http://i.qkme.me/353dum.jpg')
+
+    res = quickmeme.get('http://t.qkme.me/353dum.jpg')
+    res.should include(url: 'http://i.qkme.me/353dum.jpg')
+
+    res = quickmeme.get('http://www.quickmeme.com/meme/353dum/')
+    res.should include(url: 'http://i.qkme.me/353dum.jpg')
+
+    res = quickmeme.get('http://qkme.me/353i1m?id=189959962')
+    res.should include(url: 'http://i.qkme.me/353i1m.jpg')
+
+    res = quickmeme.get('http://qkme.me/353dum')
+    res.should include(url: 'http://i.qkme.me/353dum.jpg')
+
+    lambda { quickmeme.get('http://qkme.me/353i1ma') }.should raise_error(Thornbed::NotValid)
+    lambda { quickmeme.get('http://www.quickmeme.com/meme/353duma/') }.should raise_error(Thornbed::NotValid)
+    lambda { quickmeme.get('http://i.qkme.me/353duma.jpg') }.should raise_error(Thornbed::NotValid)
+  end
+
+  it "should discover Imgur provider" do
+    res = Thornbed.get('http://qkme.me/353dum')
+    res.should include(url: 'http://i.qkme.me/353dum.jpg')
+    res.should include(provider_name: "QuickMeme")
+
+    res = Thornbed.get('http://www.quickmeme.com/meme/353dum/')
+    res.should include(url: 'http://i.qkme.me/353dum.jpg')
+    res.should include(provider_name: "QuickMeme")
+
+    res = Thornbed.get('http://t.qkme.me/353dum.jpg')
+    res.should include(url: 'http://i.qkme.me/353dum.jpg')
+    res.should include(provider_name: "QuickMeme")
+
+    res = Thornbed.get('http://qkme.me/353i1m?id=189959962')
+    res.should include(url: 'http://i.qkme.me/353i1m.jpg')
+    res.should include(provider_name: "QuickMeme")
+
+    res = Thornbed.get('http://qkme.me/353dum')
+    res.should include(url: 'http://i.qkme.me/353dum.jpg')
+    res.should include(provider_name: "QuickMeme")
+  end
+end
