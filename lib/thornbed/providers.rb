49b52fe4bac9449a430049727a73f2ffda47f83b
Esteban (Eka) Feldman <esteban.feldman@gmail.com>
2012-11-20 07:02:51 -0500

    Fixes #1 providers are not auto loaded

--- lib/thornbed/providers.rb
+++ lib/thornbed/providers.rb
@@ -1,6 +1,6 @@
 require "pathname"
-
-Dir["lib/thornbed/providers/*.rb"].each do |name|
+providers = File.join(File.dirname(__FILE__), 'providers')
+Dir["#{providers}/*.rb"].each do |name|
   basename = Pathname.new(name).basename.to_s
   require "thornbed/providers/#{basename}"
 end
