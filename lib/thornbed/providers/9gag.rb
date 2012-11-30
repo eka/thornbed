d7ba1165792639cf9d0296f90bffd4aa55a447be
Esteban (Eka) Feldman <esteban.feldman@gmail.com>
2012-11-30 05:41:10 -0500

    added 9gag to providers

---
+++ lib/thornbed/providers/9gag.rb
@@ -0,0 +1,32 @@
+require "uri"
+require "thornbed"
+
+module Thornbed::Providers
+  class NineGag < Thornbed::Providers::Provider
+    attr_accessor :pattern
+
+    def initialize
+      self.pattern = /^http(s)?:\/\/(\w+\.)?(9gag\.com\/|d24w6bsrhbeh9d\.cloudfront\.net\/photo\/)(gag\/|fast#)?(\d+)(\?[\w=]+)?(_\d+b\.jpg)?$/
+    end
+
+    def get(url)
+      raise Thornbed::NotValid, url if !valid?(url)
+      url = URI.parse(url)
+      ptype = "jpg"
+
+      pic_id = url.path == '/fast' ? url.fragment : /(\d+)/.match(url.path)[0]
+
+      {
+        url: "http://d24w6bsrhbeh9d.cloudfront.net/photo/#{pic_id}_700b.#{ptype}",
+        type: "photo",
+        provider_name: provider_name,
+        provider_url: "http://9gag.com",
+        thumbnail_url: "http://d24w6bsrhbeh9d.cloudfront.net/photo/#{pic_id}_fbthumbnail.#{ptype}",
+        version: "1.0",
+        page: "http://9gag.com/gag/#{pic_id}",
+        width: nil,
+        height: nil
+      }
+    end
+  end
+end
---
+++ spec/providers/9gag_spec.rb
@@ -0,0 +1,44 @@
+require "spec_helper"
+
+describe "NineGag" do
+  it "should have NineGag as a provider" do
+    Thornbed::Providers.constants.should include(:NineGag)
+  end
+
+  it "should accept different NineGag urls" do
+    ninegag = Thornbed::Providers::NineGag.new
+    ninegag.valid?("http://9gag.com/gag/5931764").should be_true
+    ninegag.valid?("http://d24w6bsrhbeh9d.cloudfront.net/photo/5931764_700b.jpg").should be_true
+    ninegag.valid?("http://9gag.com/gag/5929873?ref=popular").should be_true
+    ninegag.valid?("http://9gag.com/fast#5931764").should be_true
+  end
+
+  it "should return OEmbed hash for NineGag urls" do
+    ninegag = Thornbed::Providers::NineGag.new
+    res = ninegag.get("http://9gag.com/gag/5931764")
+    res.should include(url: "http://d24w6bsrhbeh9d.cloudfront.net/photo/5931764_700b.jpg")
+
+    res = ninegag.get("http://d24w6bsrhbeh9d.cloudfront.net/photo/5931764_700b.jpg")
+    res.should include(url: "http://d24w6bsrhbeh9d.cloudfront.net/photo/5931764_700b.jpg")
+
+    res = ninegag.get("http://9gag.com/gag/5929873?ref=popular")
+    res.should include(url: "http://d24w6bsrhbeh9d.cloudfront.net/photo/5929873_700b.jpg")
+
+    res = ninegag.get("http://9gag.com/fast#5931764")
+    res.should include(url: "http://d24w6bsrhbeh9d.cloudfront.net/photo/5931764_700b.jpg")
+  end
+
+  it "should discover NineGag provider" do
+    res = Thornbed.get("http://9gag.com/gag/5931764")
+    res.should include(url: "http://d24w6bsrhbeh9d.cloudfront.net/photo/5931764_700b.jpg")
+    res.should include(thumbnail_url: "http://d24w6bsrhbeh9d.cloudfront.net/photo/5931764_fbthumbnail.jpg")
+    res.should include(page: "http://9gag.com/gag/5931764")
+    res.should include(provider_name: "NineGag")
+
+    res = Thornbed.get("http://9gag.com/fast#5931764")
+    res.should include(url: "http://d24w6bsrhbeh9d.cloudfront.net/photo/5931764_700b.jpg")
+    res.should include(thumbnail_url: "http://d24w6bsrhbeh9d.cloudfront.net/photo/5931764_fbthumbnail.jpg")
+    res.should include(page: "http://9gag.com/gag/5931764")
+    res.should include(provider_name: "NineGag")
+  end
+end
